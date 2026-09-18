5f20c7f9909f512fed1d9871a72d69a607467be1
eddie davis <eduardodavis04@csu.fullerton.edu>
2026-09-17 20:02:15 -0700

    Added health controller Skeleton

---
+++ backend/Soundwaves.Api/Controllers/HealthController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Soundwaves.Api.Controllers
+{
+    [ApiController]
+    [Route("api/health")]
+    public class HealthController : ControllerBase
+    {
+        
+    }
+}
